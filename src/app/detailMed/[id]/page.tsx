6c431d2a6bb7aea416d009c7c5201b2d8e8de30b
DiegoHVP <dhvp10@gmail.com>
2024-08-01 16:18:51 -0300

    teste

--- src/app/detailMed/[id]/page.tsx
+++ src/app/detailMed/[id]/page.tsx
@@ -24,12 +24,16 @@ export default function DetailMed({ params }: { params: { id: string } }) {
   const [med, setMed] = useState<Medicamento>();
   const [pathID, setPathID] = useState("");
 
+
+  // SE TIVER PASSADO UM ID, ADD id EM pathID
   useEffect(() => {
     if (params && params.id) {
       setPathID(params.id);
     }
   }, [params]);
 
+
+  // BUSCA O MEDICAMENTO
   const buscaDetalhes = async (id: number) => {
     setLoading(true);
     console.log(id);
@@ -53,12 +57,14 @@ export default function DetailMed({ params }: { params: { id: string } }) {
     }
   };
 
+  // APOS CARREGAR A PAGINAS FAZ A BUSCA
   useEffect(() => {
     if (pathID) {
       buscaDetalhes(parseInt(pathID)); // Chamada da função aqui
     }
   }, [pathID]); // Dependência adicionada
 
+  // ENQUANTO ESTA BUSCANDO
   if (loading) {
     return (
       <div className="container my-5">
@@ -69,6 +75,8 @@ export default function DetailMed({ params }: { params: { id: string } }) {
     );
   }
 
+
+  //VERIFICA SE TEM MEDICAMENTO
   if (!med) {
     return (
       <div className="container my-5">
@@ -78,8 +86,8 @@ export default function DetailMed({ params }: { params: { id: string } }) {
       </div>
     );
   }
-
-  return (
+  else {
+    return (
     <div className="container my-5">
       <div className="card">
         <div className="card-body">
@@ -129,4 +137,5 @@ export default function DetailMed({ params }: { params: { id: string } }) {
       </div>
     </div>
   );
+}
 }
