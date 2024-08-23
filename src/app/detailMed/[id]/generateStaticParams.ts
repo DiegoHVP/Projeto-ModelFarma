0146c3a2ea66584817f9548fbfc20549b13dc5e3
DiegoHVP <dhvp10@gmail.com>
2024-08-23 14:18:51 -0300

    add docker

--- src/app/detailMed/[id]/generateStaticParams.ts
+++
@@ -1,8 +0,0 @@
-const mockIDs = [1];
-
-// Gerando parâmetros estáticos a partir dos IDs mockados
-export async function generateStaticParams() {
-  return mockIDs.map(id => ({
-    id: id.toString()
-  }));
-}
