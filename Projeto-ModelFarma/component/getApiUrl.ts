3c1f26d9f6e488c174553c41bf3a60e21a9d1f0b
DiegoHVP <dhvp10@gmail.com>
2024-08-24 20:48:44 -0300

    fix use VAR NEXT_PUBLIC_API_URL

--- Projeto-ModelFarma/component/getApiUrl.ts
+++ Projeto-ModelFarma/component/getApiUrl.ts
@@ -1,3 +1,4 @@
 export const getApiUrl = () => {
-  return process.env.NEXT_API_URL;
+  const url = process.env.NEXT_PUBLIC_API_URL;
+  return url?.toString; // Valor padrão caso a variável de ambiente não esteja definida
 };
