3ad5ab92f724259b71fe5dc6acdf6aaa93f52731
DiegoHVP <dhvp10@gmail.com>
2024-08-25 14:17:35 -0300

    up '

--- Projeto-ModelFarma/component/getApiUrl.ts
+++ Projeto-ModelFarma/component/getApiUrl.ts
@@ -1,4 +1,5 @@
 export const getApiUrl = () => {
   const url = process.env.NEXT_PUBLIC_API_URL? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:8000';
+  console.log(String(url));
   return String(url);
 };
